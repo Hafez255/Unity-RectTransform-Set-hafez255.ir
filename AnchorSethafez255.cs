227958a96adafb65d6aa0c92a172926ee777b971
Hafez255gs <64715455+Hafez255@users.noreply.github.com>
2020-05-03 06:17:29 +0430

    Add files via upload
    //1- Create Editor Folder
    //2- Put AnchorSethafez255 in Editor Folder "Assets/Editor/AnchorSethafez255.cs"
    //3- Select Object in Scene And Press Button "H" To Automatically Set RectTransform
    //4- Contact Us: hafez255.ir@gmail.com

--- AnchorSethafez255.cs
+++ AnchorSethafez255.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 
 
 //1- Create Editor Folder
-//2- Put AnchorSethafez255 in Editor Folder
+//2- Put AnchorSethafez255 in Editor Folder "Assets/Editor/AnchorSethafez255.cs"
 //3- Select Object in Scene And Press Button "H" To Automatically Set RectTransform
 //4- Contact Us: hafez255.ir@gmail.com
 
